855e62a50206b23ecd4e130b48713af520eb32f0
Carlos Emanuel Silva e Melo Oliveira <carlos.olvr32@gmail.com>
2023-09-17 16:45:35 -0300

    Inicializando projeto Spring

---
+++ VGS/src/main/java/project/system/VGS/VgsApplication.java
@@ -0,0 +1,13 @@
+package project.system.VGS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VgsApplication.class, args);
+	}
+
+}
---
+++ VGS/src/test/java/project/system/VGS/VgsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.system.VGS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
